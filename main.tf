280bbd69a08e05d741f9d306b6eaf6d7bf3b4e4b
Aaron Ayerdis <aaronayerdis89@gmail.com>
2023-10-06 22:39:20 +0000

    #19 change structure of root module and tag bucket

--- main.tf
+++ main.tf
@@ -1,32 +1,3 @@
-terraform {
-  cloud {
-    organization = "aea"
-
-    workspaces {
-      name = "terra-house-soccer"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
@@ -41,9 +12,5 @@ resource "aws_s3_bucket" "example" {
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
 
-}
-
-output "random_bucket_name_" {
-  value = random_string.bucket_name.result
 }
 
