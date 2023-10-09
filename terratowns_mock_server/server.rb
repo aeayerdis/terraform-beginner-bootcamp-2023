d1022b87fd33b67a9beb4121bd3ccb51ab05d9a4
Aaron Ayerdis <aaronayerdis89@gmail.com>
2023-10-09 18:22:06 +0000

    #37 fixed endpoints

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -195,7 +195,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -204,9 +203,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
@@ -227,8 +226,10 @@ class TerraTownsMockServer < Sinatra::Base
       error 404, "failed to find home with provided uuid and bearer token"
     end
 
-    $home = {}
-    { message: "House deleted successfully" }.to_json
+     # delete from mock database
+     uuid = $home[:uuid]
+     $home = {}
+     { uuid: uuid }.to_json
   end
 end
 
