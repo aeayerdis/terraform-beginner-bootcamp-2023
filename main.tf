34fa13f7904e19a9bfaa4400ae48be0ada3150e1
Aaron Ayerdis <108297740+aeayerdis@users.noreply.github.com>
2023-10-21 15:16:45 -0400

    #44 test our terra home on terratowns test town (#45)

--- main.tf
+++ main.tf
@@ -8,31 +8,34 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid= var.teacherseat_user_uuid
+  token=var.terratowns_access_token
+  }
+
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
 }
 
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   content_version = var.content_version
-#   assets_path = var.assets_path
-# }
-
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "How to git gud at soccer"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Soccer is so much more than just running and kicking a ball.
+It doesn't matter if you're really athletic, if you can follow some basic steps,
+it will help you become a better player. Communication is key. Not just when you have
+the ball but when you're off the ball as well. Letting your fellow players know what is available
+to them and where the defenders are coming in from. Movement off the ball is another key thing
+in soccer. Just because you don't have the ball doesn't mean you don't have to make yourself available
+to your players who do have the ball. You must always anticipate the sudden shift in the game to be in the 
+right positioning!
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "123454.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
